ca6e5105087b80fb2fbea7a852165b5d19cc1426
Ajdev11 <mosesogun11@gmail.com>
2022-10-09 12:24:07 -0700

    created firebase and initialized git

---
+++ src/App.js
@@ -0,0 +1,69 @@
+import { React, useState } from "react";
+import "./App.css";
+
+export default function App() {
+    //created a for loop for button numbered from 1-9
+  const createDigits = () => {
+    const digits = [];
+    for (let i = 1; i < 10; i++) {
+      digits.push(
+        <button onClick={() => updateCalc(i.toString())} key={i}>
+          {i}
+        </button>
+      );
+    }
+    return digits;
+  };
+  //initialized the state Functions
+  const [calc, setCalc] = useState("");
+  const [result, setResult] = useState("");
+  const operas = ["/", "*", "-", "+", "."];
+  //updating the state
+  const updateCalc = (value) => {
+    if (
+      (operas.includes(value) && calc === "") ||
+      (operas.includes(value) && operas.includes(calc.slice(-1)))
+    ) {
+      return;
+    }
+    setCalc(calc + value);
+    if (!operas.includes(value)) {
+      setResult(eval(calc + value).toString());
+    }
+  };
+  //initialized the = function
+  const calculate = () => {
+    setCalc(eval(calc).toString());
+  };
+  //initialized the del function
+  const delValue = () => {
+    if (calc == "") {
+      return;
+    }
+    const value = calc.slice(0, -1);
+    setCalc(value);
+  };
+  return (
+   <div className="App">
+      <div className="calculator">
+        <div className="display">
+          {result ? <span>({result})</span> : ""}
+          {calc || "0"}
+        </div>
+        <div className="operators">
+          <button onClick={() => updateCalc("/")}>/</button>
+          <button onClick={() => updateCalc("*")}>*</button>
+          <button onClick={() => updateCalc("+")}>+</button>
+          <button onClick={() => updateCalc("-")}>-</button>
+          <button onClick={delValue}>DEL</button>
+        </div>
+        <div className="digits">
+          {createDigits()}
+          <button onClick={() => updateCalc("0")}>0</button>
+          <button onClick={() => updateCalc(".")}>.</button>
+          <button onClick={calculate}>=</button>
+        </div>
+      </div>
+    </div>
+  );
+}
